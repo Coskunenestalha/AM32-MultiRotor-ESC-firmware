f2354420ff54ad2c4c2b8e5d246e68f3ba5f4a1b
Coskunenestalha <100258147+Coskunenestalha@users.noreply.github.com>
2024-09-19 11:44:38 +0300

    Update rpm
    rpm deg

--- Src/main.c
+++ Src/main.c
@@ -384,8 +384,8 @@ uint16_t tim1_arr = TIM1_AUTORELOAD;         // current auto reset value
 uint16_t TIMER1_MAX_ARR = TIM1_AUTORELOAD;      // maximum auto reset register value
 uint16_t duty_cycle_maximum = TIM1_AUTORELOAD;     //restricted by temperature or low rpm throttle protect
 uint16_t low_rpm_level  = 20;        // thousand erpm used to set range for throttle resrictions
-uint16_t high_rpm_level = 70;      //
-uint16_t throttle_max_at_low_rpm  = 405;
+uint16_t high_rpm_level = 75;      //
+uint16_t throttle_max_at_low_rpm  = 400;
 uint16_t throttle_max_at_high_rpm = TIM1_AUTORELOAD;
 
 uint16_t commutation_intervals[6] = {0};
