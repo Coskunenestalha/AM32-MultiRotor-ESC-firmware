b7f76075768a7d2af868664314bf2b142fd14b66
Coskunenestalha <100258147+Coskunenestalha@users.noreply.github.com>
2024-10-10 20:07:38 +0300

    Update main.c

--- Src/main.c
+++ Src/main.c
@@ -354,8 +354,8 @@ int16_t actual_current = 0;
 
 char lowkv = 0;
 
-uint16_t min_startup_duty = 120;
-uint16_t sin_mode_min_s_d = 120;
+uint16_t min_startup_duty = 1000;
+uint16_t sin_mode_min_s_d = 1000;
 char bemf_timeout = 10;
 
 char startup_boost = 50;
@@ -374,9 +374,9 @@ typedef enum
   GPIO_PIN_SET
 }GPIO_PinState;
 
-uint16_t startup_max_duty_cycle = 300 + DEAD_TIME;
-uint16_t minimum_duty_cycle = DEAD_TIME;
-uint16_t stall_protect_minimum_duty = DEAD_TIME;
+uint16_t startup_max_duty_cycle = 1000 + DEAD_TIME;
+uint16_t minimum_duty_cycle = 300+DEAD_TIME;
+uint16_t stall_protect_minimum_duty =300+ DEAD_TIME;
 char desync_check = 0;
 char low_kv_filter_level = 20;
 
@@ -635,11 +635,11 @@ void loadEEpromSettings(){
 	    }
 
 	   if(eepromBuffer[25] < 151 && eepromBuffer[25] > 49){
-	   min_startup_duty = (eepromBuffer[25] + DEAD_TIME) * TIMER1_MAX_ARR / 2000;
-	   minimum_duty_cycle = (eepromBuffer[25]/ 2 + DEAD_TIME/3) * TIMER1_MAX_ARR / 2000 ;
-	   stall_protect_minimum_duty = minimum_duty_cycle+10;
+	   min_startup_duty = 800 * TIMER1_MAX_ARR / 2000;
+	   minimum_duty_cycle = (800/ 2 + DEAD_TIME/3) * TIMER1_MAX_ARR / 2000 ;
+	   stall_protect_minimum_duty = minimum_duty_cycle+200;
 	    }else{
-	    	min_startup_duty = 150;
+	    	min_startup_duty = 800;
 	    	minimum_duty_cycle = (min_startup_duty / 2) + 10;
 	    }
       motor_kv = (eepromBuffer[26] * 40) + 20;
@@ -706,8 +706,8 @@ void loadEEpromSettings(){
 	   if(dead_time_override > 200){
 	   dead_time_override = 200;
 	   }
-	   min_startup_duty = eepromBuffer[25] + dead_time_override;
-	   minimum_duty_cycle = eepromBuffer[25]/2 + dead_time_override;
+	   min_startup_duty = 900 + dead_time_override;
+	   minimum_duty_cycle = 900/2 + dead_time_override;
 	   throttle_max_at_low_rpm  = throttle_max_at_low_rpm + dead_time_override;
 	   startup_max_duty_cycle = startup_max_duty_cycle  + dead_time_override;
 	   TIM1->BDTR |= dead_time_override;
