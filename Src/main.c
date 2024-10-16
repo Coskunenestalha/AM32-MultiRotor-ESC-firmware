3475c941d078996077bbe1ad6e11eae210af119d
Coskunenestalha <100258147+Coskunenestalha@users.noreply.github.com>
2024-10-16 18:57:15 +0300

    Update main.c

--- Src/main.c
+++ Src/main.c
@@ -354,8 +354,8 @@ int16_t actual_current = 0;
 
 char lowkv = 0;
 
-uint16_t min_startup_duty = 120;
-uint16_t sin_mode_min_s_d = 120;
+uint16_t min_startup_duty = 480;
+uint16_t sin_mode_min_s_d = 480;
 char bemf_timeout = 10;
 
 char startup_boost = 50;
@@ -635,11 +635,11 @@ void loadEEpromSettings(){
 	    }
 
 	   if(eepromBuffer[25] < 151 && eepromBuffer[25] > 49){
-	   min_startup_duty = (1800 + DEAD_TIME) * TIMER1_MAX_ARR / 2000;
+	   min_startup_duty = (480 + DEAD_TIME) * TIMER1_MAX_ARR / 2000;
 	   minimum_duty_cycle = (1800/ 2 + DEAD_TIME/3) * TIMER1_MAX_ARR / 2000 ;
 	   stall_protect_minimum_duty = minimum_duty_cycle+10;
 	    }else{
-	    	min_startup_duty = 150;
+	    	min_startup_duty = 480;
 	    	minimum_duty_cycle = (1800 / 2) + 10;
 	    }
       motor_kv = (eepromBuffer[26] * 40) + 20;
@@ -706,7 +706,7 @@ void loadEEpromSettings(){
 	   if(dead_time_override > 200){
 	   dead_time_override = 200;
 	   }
-	   min_startup_duty = 1800 + dead_time_override;
+	   min_startup_duty = 480 + dead_time_override;
 	   minimum_duty_cycle = 1800/2 + dead_time_override;
 	   throttle_max_at_low_rpm  = throttle_max_at_low_rpm + dead_time_override;
 	   startup_max_duty_cycle = startup_max_duty_cycle  + dead_time_override;
