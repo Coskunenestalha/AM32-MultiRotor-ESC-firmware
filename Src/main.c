178bd230568354f861d08dc1e32d5d5a44e31913
Coskunenestalha <100258147+Coskunenestalha@users.noreply.github.com>
2024-11-21 11:03:15 +0300

    Update main.c

--- Src/main.c
+++ Src/main.c
@@ -354,11 +354,11 @@ int16_t actual_current = 0;
 
 char lowkv = 0;
 
-uint16_t min_startup_duty = 2000;
-uint16_t sin_mode_min_s_d = 2000;
+uint16_t min_startup_duty = 120;
+uint16_t sin_mode_min_s_d = 120;
 char bemf_timeout = 10;
 
-char startup_boost = 150;
+char startup_boost = 50;
 char reversing_dead_band = 1;
 
 uint16_t low_pin_count = 0;
@@ -374,7 +374,7 @@ typedef enum
   GPIO_PIN_SET
 }GPIO_PinState;
 
-uint16_t startup_max_duty_cycle = 2000 + DEAD_TIME;
+uint16_t startup_max_duty_cycle = 300 + DEAD_TIME;
 uint16_t minimum_duty_cycle = DEAD_TIME;
 uint16_t stall_protect_minimum_duty = DEAD_TIME;
 char desync_check = 0;
@@ -635,12 +635,12 @@ void loadEEpromSettings(){
 	    }
 
 	   if(eepromBuffer[25] < 151 && eepromBuffer[25] > 49){
-	   min_startup_duty = (2000 + DEAD_TIME) * TIMER1_MAX_ARR / 2000;
+	   min_startup_duty = (eepromBuffer[25] + DEAD_TIME) * TIMER1_MAX_ARR / 2000;
 	   minimum_duty_cycle = (eepromBuffer[25]/ 2 + DEAD_TIME/3) * TIMER1_MAX_ARR / 2000 ;
 	   stall_protect_minimum_duty = minimum_duty_cycle+10;
 	    }else{
-	    	min_startup_duty = 2000;
-	    	minimum_duty_cycle = (300 / 2) + 10;
+	    	min_startup_duty = 150;
+	    	minimum_duty_cycle = (min_startup_duty / 2) + 10;
 	    }
       motor_kv = (eepromBuffer[26] * 40) + 20;
       motor_poles = eepromBuffer[27];
@@ -706,7 +706,7 @@ void loadEEpromSettings(){
 	   if(dead_time_override > 200){
 	   dead_time_override = 200;
 	   }
-	   min_startup_duty = 2000 + dead_time_override;
+	   min_startup_duty = eepromBuffer[25] + dead_time_override;
 	   minimum_duty_cycle = eepromBuffer[25]/2 + dead_time_override;
 	   throttle_max_at_low_rpm  = throttle_max_at_low_rpm + dead_time_override;
 	   startup_max_duty_cycle = startup_max_duty_cycle  + dead_time_override;
@@ -1088,8 +1088,8 @@ if(!armed && (cell_count == 0)){
 
 		  	 if(stall_protection && running ){  // this boosts throttle as the rpm gets lower, for crawlers and rc cars only, do not use for multirotors.
 		  		 stall_protection_adjust += (doPidCalculations(&stallPid, commutation_interval, stall_protect_target_interval))/10000;
-		  					 if(stall_protection_adjust > 2500){
-		  						stall_protection_adjust = 2500;
+		  					 if(stall_protection_adjust > 2000){
+		  						stall_protection_adjust = 2000;
 		  					 }
 		  					 if(stall_protection_adjust <= 0){
 		  						stall_protection_adjust = 0;
